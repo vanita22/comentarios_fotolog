1506eb14b7982b401c370f1b1f9ab3ce40469685
vanita22 <vanitayrafita2004@gmail.com>
2017-06-18 18:00:38 -0300

    subiendo archivos finales

--- js/main.js
+++ js/main.js
@@ -20,12 +20,12 @@ function guardar(){
 		var valuee = localStorage.getItem(kei);
 		
 
-		var post = document.createElement("h4");
+		var post = document.createElement("h4");/*con este Dom se imprime por pantalla lo que se ingresa en el input en el nombre y se envía a la caja llamada contenedor*/
 		var posteo = document.createTextNode(kei);
 		post.appendChild(posteo);
 		contenedor.appendChild(post);	
 
-		var post2 = document.createElement("p");
+		var post2 = document.createElement("p");/*con este Dom se imprime por pantalla lo que se ingresa en el input en el comentario y se envía a la caja llamada contenedor*/
 		var posteo2 = document.createTextNode(valuee);
 		post2.appendChild(posteo2);
 		contenedor.appendChild(post2);	
@@ -33,10 +33,9 @@ function guardar(){
 		/*contenedor.innerHTML += "<h4>" + "<strong>" + kei + "</strong>" + "</h4>" + "<p>" + valuee + "</p>";/*se usa += para agregar, si se usa == se sobreescribe el valor*/
 		/*contenedor.style.backgroundColor = '#DF33B2'; */
 	}
-
 }
 
-function borrar(){
+function borrar(){  /*sirve para borrar todo el contenido de a caja contenedor con el botón borrar*/
 	localStorage.clear();
 	guardar(); 
 }
